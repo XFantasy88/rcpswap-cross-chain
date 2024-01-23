2deed469de11375e70c24e37e9b7d8d90cb2ef9d
XFantasy88 <erencote@gmail.com>
2024-01-23 13:22:02 -0500

    update

--- apps/web/src/ui/swap/swap-trade-button.tsx
+++ apps/web/src/ui/swap/swap-trade-button.tsx
@@ -540,6 +540,9 @@ export default function SwapTradeButton() {
             ? "Amount is too low"
             : (symbiosisError as any)?.code === ErrorCode.AMOUNT_TOO_HIGH
             ? "Amount is too high"
+            : ((symbiosisError as any)?.message?.indexOf("limit reached") ??
+                -1) > -1
+            ? "Limit reached, try again later"
             : "Invalid trade, try lower amounts"
           : undefined
       }
